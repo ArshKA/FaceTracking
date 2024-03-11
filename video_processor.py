1e5b37d6507a4326497c322b8669972ecc339ace
arshkon <arshkon@gmail.com>
2024-03-10 17:59:49 -0700

    video processing

---
+++ video_processor.py
@@ -0,0 +1,69 @@
+from mtcnn import MTCNN
+from deepface import DeepFace
+import cv2
+import imutils
+from tqdm import tqdm
+
+import numpy as np
+from clustering import cluster_and_plot, cluster_and_plot_gmm, cluster_and_plot_optics, cluster_and_plot_dpgmm
+
+
+
+detector = MTCNN()
+
+def process_video(path):
+
+
+
+    cap = cv2.VideoCapture(path)
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    i = 0
+
+    # Get total frames
+
+    all_embeddings = []
+    predictions = []
+    history = {}
+    face_counts = []
+
+    with tqdm(total=total_frames) as pbar:
+        while cap.isOpened():
+            ret, frame = cap.read()
+
+            if not ret or i > 10:
+                break
+
+            frame = imutils.resize(frame, width=1024)
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+            detections = detector.detect_faces(frame)
+            face_counts.append(0)
+            history[i] = []
+            for detection in detections:
+                confidence = detection["confidence"]
+                if confidence > 0.8:
+                    x, y, w, h = detection["box"]
+                    detected_face = frame[int(y):int(y+h), int(x):int(x+w)]
+
+                    embedding = np.array(DeepFace.represent(detected_face, model_name='Facenet512', enforce_detection=False)[0]['embedding'])
+                    all_embeddings.append(embedding)
+                    predictions.append(np.random.random(10))
+                    # all_embeddings.append(np.append(embedding, i/5))
+                    face_counts[-1] += 1
+                    history[i].append((len(all_embeddings)-1, (x, y, w, h)))
+
+            pbar.update(1)
+            i += 1
+
+    # Release resources
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+
+    return all_embeddings, predictions, face_counts
+
+
+
+
